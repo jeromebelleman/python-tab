4a4f834f50ce7caf72c6f9b99610961312efbbd9
Jerome Belleman <jerome.belleman@gmail.com>
2017-05-14 16:11:02 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
+# coding=utf-8
 
+import os
 from distutils.core import setup
 
-setup(name='tab',
-      py_modules=['tab'],
-     )
+delattr(os, 'link')
+
+setup(
+    name='python-tab',
+    version='1.0',
+    author='Jerome Belleman',
+    author_email='Jerome.Belleman@gmail.com',
+    url='http://cern.ch/jbl',
+    description="Print tables",
+    long_description="Lay out tables with sensible column widths and print them.",
+    py_modules=['tab'],
+)
